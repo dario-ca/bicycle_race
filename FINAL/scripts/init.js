80964ca2e5b301b7e617be41238d29ac2bfd3e12
Paolo Bruzzo <pbruzz2@uic.edu>
2014-10-09 18:35:53 -0500

    4 graph at a time

--- FINAL/scripts/init.js
+++ FINAL/scripts/init.js
@@ -17,9 +17,9 @@ function init(DOC) {
 	drawGraph(graphid);
 	drawQuery_Calendar(queryoptionid);
     bar_App1 = new BarChart1(chart1);
-    bar_App2 = new LineChart2(chart2);
-    bar_App3 = new BarChart3(chart3);
-    bar_App4 = new BarChart2(chart4);
+    bar_App2 = new BarChart2(chart3);
+    bar_App3 = new BarChart3(chart2);
+    bar_App4 = new LineChart2(chart4);
 	
 	console.log("Done");
 
