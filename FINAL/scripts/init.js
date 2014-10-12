793876d7510d435a9128ccb811c27da5d9742f56
Paolo Bruzzo <pbruzz2@uic.edu>
2014-10-12 12:24:04 -0500

    Code cleanup

--- FINAL/scripts/init.js
+++ FINAL/scripts/init.js
@@ -3,25 +3,19 @@ function init(DOC) {
 	console.log("Initialize Page...");
 	
 	/* Getting ID's from HTML document DOC */
-	mapid = DOC.getElementById("map");
-	graphiconid = DOC.getElementById("graphicons");
-	graphid = DOC.getElementById("graph");
-	queryoptionid = DOC.getElementById("options");
+	mapid			= DOC.getElementById("map");
+	graphiconid		= DOC.getElementById("graphicons");
+	graphid			= DOC.getElementById("graph");
+	queryoptionid	= DOC.getElementById("options");
     
-    chart1 = DOC.getElementById("chart1");
-    chart2 = DOC.getElementById("chart2");
-    chart3 = DOC.getElementById("chart3");
-    chart4 = DOC.getElementById("chart4");
 	
 	/* Calling functions */
     BikeMap.init(mapid);
 	drawGraphIcons(graphiconid);
-	drawGraph(graphid);
-	drawQuery_Calendar(queryoptionid);
-    app1 = new LineChart1(chart1,DOC.getElementById("titlebar1"));
-    app2 = new BarChart1(chart2,DOC.getElementById("titlebar2"));
-    app3 = new BarChart4(chart3, DOC.getElementById("titlebar3"));
-    app4 = new LineChart2(chart4,DOC.getElementById("titlebar4"));
+	selectGraph(DOC,1);
+	// drawQuery_Calendar(queryoptionid);
+	// drawQueryMenu(queryoptionid);
+    
 	
 	console.log("Done");
 
