aca9fc749582f75b54ab1c79c040df63ba547c03
Paolo Bruzzo <pbruzz2@uic.edu>
2014-10-12 15:47:18 -0500

    basic filters

--- FINAL/scripts/init.js
+++ FINAL/scripts/init.js
@@ -4,14 +4,16 @@ function init(DOC) {
 	
 	/* Getting ID's from HTML document DOC */
 	mapid			= DOC.getElementById("map");
-	graphiconid		= DOC.getElementById("graphicons");
+	graphicon_gender= DOC.getElementById("graphicons_gender");
+    graphicon_utype = DOC.getElementById("graphicons_utype");
 	graphid			= DOC.getElementById("graph");
 	queryoptionid	= DOC.getElementById("options");
     
 	
 	/* Calling functions */
     BikeMap.init(mapid);
-	drawGraphIcons(graphiconid);
+	drawGraphIcons(graphicon_gender);
+    drawGraphIcons(graphicon_utype);
 	selectGraph(DOC,1);
 	// drawQuery_Calendar(queryoptionid);
 	// drawQueryMenu(queryoptionid);
