59fa811de46d019cc676ebf485664d556c79509d
Dario Casula <dario.bs91@gmail.com>
2014-10-16 10:18:21 -0500

    zoom to fix

--- FINAL/charts/bikes_by_hour.js
+++ FINAL/charts/bikes_by_hour.js
@@ -102,7 +102,7 @@ LineChart1.prototype.draw = function () {
     var zoom = d3.behavior.zoom()
         //.x(xScale)
         .y(yScale)
-        .scaleExtent([1, 1.5])
+        .scaleExtent([1, 2])
         .on("zoom", zoomed);	
 
     var svg = this.svg;
@@ -173,8 +173,8 @@ LineChart1.prototype.draw = function () {
         
         svg.select(".y.axis").call(yAxis);   
         
-        svg.selectAll(".chart.line").attr('d', line)
-            .attr("transform", "translate(" + zoom.translate()+")"+"scale(" + zoom.scale() + ")");
+        svg.selectAll(".chart.line").attr('d', line);
+           // .attr("transform", "translate(" + zoom.translate()+")"+"scale(" + zoom.scale() + ")");
         
         gy.selectAll("g")
             .classed("yminor", true)
@@ -183,12 +183,12 @@ LineChart1.prototype.draw = function () {
             return width;
         });
         
-        /*gx.selectAll("text")
-            .attr("transform","rotate(-35)")
-            .style("text-anchor", "end");*/
-        
         gx.selectAll("text")
-            .attr("transform", "translate(" + zoom.translate()+") scale(" + zoom.scale() + ") rotate(-35)");
+            .attr("transform","rotate(-35)")
+            .style("text-anchor", "end");
+        
+        /*gx.selectAll("text")
+            .attr("transform", "translate("+zoom.translate()+")"+"scale("+zoom.scale()+")"+"rotate(-35)");*/
         
         /*gx.selectAll("text")        
             .attr("transform", "translate(" + d3.event.translate[0]+",0) rotate(-35)")
